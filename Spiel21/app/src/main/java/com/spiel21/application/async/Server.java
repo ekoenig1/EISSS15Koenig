20c243473e17ebb30b1a7278783a143e1bb2cf21
Eduard Koenig <eduard.koenig@smail.fh-koeln.de>
2015-06-26 15:58:08 +0200

    MS Updates

--- Spiel21/app/src/main/java/com/spiel21/application/async/Server.java
+++
@@ -1,9 +0,0 @@
-package com.spiel21.application.async;
-
-public class Server {
-
-    // TODO: hier immer die aktuelle IP-Adresse eintragen
-    public static String getAdresse () {
-        return "http://3dc8e410.ngrok.io";     // Bsp: http://10.0.2.2:3000/
-    }
-}
